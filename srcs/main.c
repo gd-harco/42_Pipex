f8376b8ee0857c95e14462ee8bfa666d87d5cef5
gd-harco <gd-harco@student.42lyon.fr>
2023-02-11 21:47:20 +0100

    test execve, not working

--- srcs/main.c
+++ srcs/main.c
@@ -3,20 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gd-harco <gd-harco@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 15:37:49 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/02/10 15:37:49 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/02/11 21:39:31 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include "../libft/includes/libft.h"
+#include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
-int main(void)
+int	main(void)
 {
-	for (int i = 0; i < 250; i++)
-		ft_printf("Error number : %i, Error message : %s\n",i, strerror(i));
+	char	*argv[4];
+	char	path[] = "./push_swap.out";
+
+	argv[0] = path;
+	argv[1] = "59";
+	argv[2] = "29";
+	argv[3] = "69";
+	if (execve(path, argv, __environ) == -1)
+	{
+		ft_printf("%s\n", strerror(errno));
+	}
 	return (0);
 }
