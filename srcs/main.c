9deefd073f483e9a8eb5e04e641c825587480a70
gd-harco <gd-harco@student.42lyon.fr>
2023-02-15 15:02:41 +0100

    recup path okay, mise a la norme

--- srcs/main.c
+++ srcs/main.c
@@ -6,31 +6,17 @@
 /*   By: gd-harco <gd-harco@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 11:18:35 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/02/15 12:47:48 by gd-harco         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/10 15:37:49 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/02/13 22:21:07 by gd-harco         ###   ########lyon.fr   */
+/*   Updated: 2023/02/15 15:02:23 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv, char *envp[])
+char	**get_path(char **envp)
 {
 	char	*path_str;
-	char	**paths;
-	char	*cur_path;
-	char	**new_argv;
 	int		index;
+	char	**result;
 
 	index = 0;
 	path_str = NULL;
@@ -44,20 +30,35 @@ int	main(int argc, char **argv, char *envp[])
 		index++;
 	}
 	if (!path_str)
-		return (1);
+		exit (1);
+	result = (ft_split(path_str, ':'));
+	free(path_str);
+	return (result);
+}
+
+int	main(int argc, char **argv, char *envp[])
+{
+	char	**paths;
+	char	*cur_path;
+	char	**new_argv;
+	int		index;
+
 	index = 0;
-	paths = ft_split(path_str, ':');
-	free (path_str);
+	paths = get_path(envp);
 	new_argv = malloc(sizeof (char *) * (argc + 1));
 	new_argv[2] = NULL;
 	cur_path = ft_strjoin(3, paths[index], "/", argv[1]);
 	while (paths[index] && access(cur_path, X_OK) != 0)
 	{
 		index++;
+		free(cur_path);
 		cur_path = ft_strjoin(3, paths[index], "/", argv[1]);
 	}
 	new_argv[0] = cur_path;
 	new_argv[1] = NULL;
+	ft_free_split(paths);
 	execve(cur_path, new_argv, envp);
+	free(cur_path);
+	free(new_argv);
 	return (0);
 }
