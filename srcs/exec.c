2dcac6bb17a6606edd6263299d013b7463cb6404
gd-harco <gd-harco@student.42lyon.fr>
2023-02-25 13:58:01 +0100

    now norm-compatible

--- srcs/exec.c
+++ srcs/exec.c
@@ -6,11 +6,13 @@
 /*   By: gd-harco <gd-harco@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 13:28:23 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/02/25 13:32:02 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/02/25 13:41:00 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+static void	initial_loop(t_pipex data, int cur_cmd, char **envp);
 
 void	cmd_exec(t_pipex data, char **envp)
 {
@@ -21,23 +23,7 @@ void	cmd_exec(t_pipex data, char **envp)
 	dup2(data.infile_fd, STDIN_FILENO);
 	while (cur_cmd < data.command_nb - 1)
 	{
-		pipe(data.pipefd);
-		pid = fork();
-		if (pid == 0)
-		{
-			close(data.pipefd[0]);
-			dup2(data.pipefd[1], STDOUT_FILENO);
-			close(data.pipefd[1]);
-			execve(data.command[cur_cmd][0], data.command[cur_cmd], envp);
-			my_perror(data.command[cur_cmd][0]);
-		}
-		else
-		{
-			wait(NULL);
-			close(data.pipefd[1]);
-			dup2(data.pipefd[0], STDIN_FILENO);
-			close(data.pipefd[0]);
-		}
+		initial_loop(data, cur_cmd, envp);
 		cur_cmd++;
 	}
 	dup2(data.outfile_fd, STDOUT_FILENO);
@@ -50,4 +36,27 @@ void	cmd_exec(t_pipex data, char **envp)
 	}
 	else
 		wait(NULL);
+}
+
+static void	initial_loop(t_pipex data, int cur_cmd, char **envp)
+{
+	pid_t	pid;
+
+	pipe(data.pipefd);
+	pid = fork();
+	if (pid == 0)
+	{
+		close(data.pipefd[0]);
+		dup2(data.pipefd[1], STDOUT_FILENO);
+		close(data.pipefd[1]);
+		execve(data.command[cur_cmd][0], data.command[cur_cmd], envp);
+		my_perror(data.command[cur_cmd][0]);
+	}
+	else
+	{
+		wait(NULL);
+		close(data.pipefd[1]);
+		dup2(data.pipefd[0], STDIN_FILENO);
+		close(data.pipefd[0]);
+	}
 }
