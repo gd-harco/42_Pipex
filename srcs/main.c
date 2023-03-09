c1f2f403eb060431a1f18a61a4ea581b500e98ec
gd-harco <gd-harco@student.42lyon.fr>
2023-03-09 12:06:48 +0100

    now exiting if the argument number isn't 5

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: gd-harco <gd-harco@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 10:47:17 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/03/08 16:51:41 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/03/09 12:04:28 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,15 @@ int	main(int argc, char **argv, char *envp[])
 	if (envp[0] == NULL)
 		exit (1);
 	if (argc < 5)
+	{
+		ft_putstr_fd("Too few argument provided, exiting\n", 2);
 		return (0);
+	}
+	if (argc > 5)
+	{
+		ft_putstr_fd("Too many argument provided, exiting\n", 2);
+		return (0);
+	}
 	parsing_full(&data, argv, envp, argc);
 	cmd_exec(data, envp);
 	clean_struct(&data);
