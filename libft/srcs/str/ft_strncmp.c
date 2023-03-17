57be4d0c9a9fb92e509afdb55cb2faf9b48d5479
Guillaume <g.dharcourt+github@posteo.net>
2023-03-17 11:45:56 +0100

    Update ft_strncmp.c

--- libft/srcs/str/ft_strncmp.c
+++ libft/srcs/str/ft_strncmp.c
@@ -21,6 +21,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	s1b = (unsigned char *)s1;
 	s2b = (unsigned char *)s2;
 	x = 0;
+//TODO ENLEVER CES DEUX LIGNES, LA FONCTION EST SENSÉ SEGFAULT. PROTÉGER PLUS TÔT DANS LE PROGRAMME 
 	if (!s1 || !s2)
 		return (-1);
 	while ((s1b[x] || s2b[x]) && x < n)
